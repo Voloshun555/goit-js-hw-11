17da5f9e419a282163343bab695dff0fff045b50
Voloshun555 <voloshunihor@gmail.com>
2023-03-30 16:19:08 +0300

    14

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import { fetchImages } from "./js/fetch-Image";
+import { renderGallary } from "./js/render-gallary";
+import { Notify } from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import NewsApiService from "./js/fetch-Image";
+
+const form = document.querySelector('.search-form');
+const submit = document.querySelector('[type="submit"]');
+const loadMoreBtn = document.querySelector('.load-more')
+const newsApiService = new NewsApiService();
+const gallary = document.querySelector('.gallery')
+
+
+
+ form.addEventListener('submit', onSearch)
+ loadMoreBtn.addEventListener('click', onLoadMore)
+
+ function onSearch (evt) {
+ evt.preventDefault();
+ newsApiService.query = evt.currentTarget.searchQuery.value.trim();
+ newsApiService.resetPage();
+ newsApiService.fetchArticles().then(renderGallary)
+ 
+ }
+function onLoadMore() {
+    newsApiService.fetchArticles().then(renderGallary)
+}
+
+
+// function alertImagesFound(data) {
+//     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//   }
+  
+//   function alertNoEmptySearch() {
+//     Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+//   }
+  
+//   function alertNoImagesFound() {
+//     Notiflix.Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.',
+//     );
+//   }
+  
+//   function alertEndOfSearch() {
+//     Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+//   }
+  
+// =======================================================================================================================================
+// const newsApiService = new NewsApiService();
+// function onSearch(e) {
+// e.preventDefault();
+// newsApiService.query = e.currentTarget.searchQuery.value.trim();
+// newsApiService.resetPage();
+// newsApiService.fetchArticles();
+
+// }
+
---
+++ src/js/render-gallary.js
@@ -0,0 +1,28 @@
+export {renderGallary}
+const gallary = document.querySelector('.gallery')
+
+function renderGallary (photo) {
+    const gallaryRander = photo
+    .map(photo => {const 
+      {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = photo
+ return `<div class="photo-card">
+ <a class="gallery__link" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </div>`
+    })
+    gallary.insertAdjacentHTML('beforeend', gallaryRander);
+};
